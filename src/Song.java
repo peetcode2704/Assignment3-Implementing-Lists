6fb1a51925ec60fd21f3a764a1f0cc2ab22f0905
Peter <peterhoang2704@gmail.com>
2025-02-12 17:58:20 -0600

    Finished Song class that represents song in spotify.

---
+++ src/Song.java
@@ -0,0 +1,12 @@
+public class Song {
+    private String title;
+    private String artist;
+    private double length;
+
+    public Song(String title,String artist,double length) {
+        this.artist = artist;
+        this.title = title;
+        this.length = length;
+    }
+
+}
