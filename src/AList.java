8163ead6e5dbab8c5f524ca508b6e8eefaf39336
Peter <peterhoang2704@gmail.com>
2025-02-14 16:32:03 -0600

    Finished AList class which contains methods to add or remove an element into the array. Added a print method to print them out.

---
+++ src/AList.java
@@ -0,0 +1,43 @@
+public class AList {
+    private int maxSize;
+    private int size;
+    private Song[] songs;
+
+    public AList() {
+        maxSize = 10;
+        size = 0;
+        songs = new Song[maxSize];
+    }
+
+    public void addy(Song p){
+        songs[size] = p;
+        if(size > maxSize){
+            maxSize *= 2;
+            Song[] newSong = new Song[maxSize];
+            for (int i = 0; i < newSong.length; i++) {
+                newSong[i] = songs[i];
+            }
+            size++;
+        }
+    }
+
+    public void removy(int pos){
+        if (pos < 0 || pos >= size) {
+            throw new IndexOutOfBoundsException("Invalid position: " + pos);
+        }
+        for (int i = pos; i < size - 1; i++) {
+            songs[i] = songs[i + 1]; //Shift the elements right to left
+        }
+        songs[size - 1] = null;
+        size--;
+    }
+
+    public String toString() {
+        String songinString = "";
+        for (int i = 0; i < size; i++) {
+            songinString += songs[i] + "\n";
+        }
+        return songinString;
+    }
+}
+
